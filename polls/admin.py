ffc57ce03169f653cd51e1b01f87433c74118595
harsh-2737 <pharsh5727@gmail.com>
2026-02-10 20:18:50 +0530

    lab-8 completed

--- polls/admin.py
+++ polls/admin.py
@@ -1,5 +1,26 @@
 from django.contrib import admin
+from .models import Question, Choice
 
-from .models import Question
 
-admin.site.register(Question)
+class ChoiceInline(admin.TabularInline):   # compact table view
+    model = Choice
+    extra = 2   # number of empty choice boxes
+
+
+class QuestionAdmin(admin.ModelAdmin):
+    # Field layout in admin form
+    fieldsets = [
+        (None, {"fields": ["question_text"]}),
+        ("Date information", {"fields": ["pub_date"], "classes": ["collapse"]}),
+    ]
+
+    # Inline choices inside Question page
+    inlines = [ChoiceInline]
+
+    # Change list page (question list)
+    list_display = ["question_text", "pub_date", "was_published_recently"]
+    list_filter = ["pub_date"]
+    search_fields = ["question_text"]
+
+
+admin.site.register(Question, QuestionAdmin)
